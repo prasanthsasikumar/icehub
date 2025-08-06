b67295cf73ae909bf9788d34e5430304aa9cd897
Prasanth Sasikumar <prasanth.sasikumar.psk@gmail.com>
2025-08-06 16:07:16 +0800

    white space issue in bio

--- pages/profile/[name].vue
+++ pages/profile/[name].vue
@@ -207,7 +207,7 @@
               <div class="space-y-4">
                 <div>
                   <h3 class="font-semibold text-gray-700 mb-2">Bio</h3>
-                  <p class="text-gray-600 leading-relaxed">{{ data.bio }}</p>
+                  <p class="text-gray-600 leading-relaxed whitespace-pre-line">{{ data.bio }}</p>
                 </div>
                 
                 <div v-if="data.email">
